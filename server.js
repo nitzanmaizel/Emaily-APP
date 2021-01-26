28552bbe9cbcc7f8ce84abcc3a6c95656a78a1a9
Nitzan Maizel <nitzanmaizel@gmail.com>
2021-01-26 17:18:13 +0200

    Change Get-age

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-	res.send({ name: 'My name is Nitzan Maizel' });
+	res.send({ age: 'My age is 27' });
 });
 
 const PORT = process.env.PORT || 5000;
