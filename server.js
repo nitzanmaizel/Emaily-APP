18da54b9b0f8a2ddce016178c875b1d671697696
Nitzan Maizel <nitzanmaizel@gmail.com>
2021-01-26 17:13:51 +0200

    Change Get

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-	res.send({ hi: 'Hello World!!!' });
+	res.send({ name: 'My name is Nitzan Maizel' });
 });
 
 const PORT = process.env.PORT || 5000;
